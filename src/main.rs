f2dd50e5395f26b49800e82e7937998fb993f70f
rubonics <rubenrodriguez+github@gmail.com>
2022-11-29 15:15:29 -0400

    attempted using the georender-pack.rs module to do the osm to georender step. fails in the same way, and the source for the latest crate isn't on github so i will move forward with the node package

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use georender_pack::{encode,Member,MemberType,MemberRole};
+use hex;
+use osmpbf::{Element, ElementReader};
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let reader = ElementReader::from_path(&args[1]).unwrap();
+
+    let mut nodes: HashMap<u64, (f32, f32)> = HashMap::new();
+    let mut ways: HashMap<u64, Vec<u64>> = HashMap::new();
+
+    reader.for_each(|item| {
+        match item {
+            Element::DenseNode(node) => {
+                let point = (node.lon() as f32, node.lat() as f32);
+                nodes.insert(node.id as u64, point);
+                // println!("debug:dense-node:id:{:?}", node.id);
+                // println!("debug:dense-node:point:{:?}", point);
+                let tags = node.tags().into_iter().collect::<Vec<_>>();
+                let encoded = encode::node(node.id as u64, point, &tags).unwrap();
+                println!("{}", hex::encode(encoded));
+            },
+            Element::Node(node) => {
+                let point = (node.lon() as f32, node.lat() as f32);
+                nodes.insert(node.id() as u64, point);
+                let tags = node.tags().into_iter().collect::<Vec<_>>();
+                // println!("debug:node:id:{:?}", node.id);
+                // println!("debug:node:point:{:?}", point);
+                let encoded = encode::node(node.id() as u64, point, &tags).unwrap();
+                println!("{}", hex::encode(encoded));
+            },
+            Element::Relation(rel) => {
+                let tags = rel.tags().into_iter().collect::<Vec<_>>();
+                let members = rel.members().map(|m| convert_member(&m)).collect::<Vec<_>>();
+                println!("debug:relation:id:{:?}", rel.id());
+                println!("debug:relation:members:{:?}", members);
+                let encoded = encode::relation(rel.id() as u64, &tags, &members, &nodes, &ways).unwrap();
+                println!("{}", hex::encode(encoded));
+            },
+            Element::Way(way) => {
+                let tags = way.tags().into_iter().collect::<Vec<_>>();
+                let refs = way.refs().map(|r| r as u64).collect::<Vec<u64>>();
+                ways.insert(way.id() as u64, refs.clone());
+                let encoded = encode::way(way.id() as u64, &tags, &refs, &nodes).unwrap();
+                println!("{}", hex::encode(encoded));
+            }
+        }
+    }).unwrap();
+    Ok(())
+}
+
+fn convert_member(m: &osmpbf::RelMember) -> Member {
+    Member::new(
+        m.member_id as u64,
+        match m.role().unwrap() {
+            "inner" => MemberRole::Inner(),
+            "outer" => MemberRole::Outer(),
+            _ => MemberRole::Unused(),
+        },
+        match m.member_type {
+            osmpbf::RelMemberType::Node => MemberType::Node(),
+            osmpbf::RelMemberType::Way => MemberType::Way(),
+            osmpbf::RelMemberType::Relation => MemberType::Relation(),
+        }
+    )
+}
